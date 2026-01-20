273b9e884c288e90b4c4aa5eb635614f2e35f6f2
farzana06tasnim <farzana06tasnim@gmail.com>
2026-01-20 21:21:12 +0800

    pengasingan kod

---
+++ biodata.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "NUR FARZANA TASNIM BINTI HUZAINI";
+$nomatrik = "18DIT24F1040";
+$kelas = "DIT4E";
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$nama = "NUR FARZANA TASNIM BINTI HUZAINI";
+$nomatrik = "18DIT24F1040";
+$kelas = "DIT4E";
+?>
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>MAKLUMAT DIRI SAYA</title>
+         <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+    <h1>PENGENALAN</h1>
+
+    <p>Nama :<?=$nama ?></p>
+    <p>No. Matrik :<?=$nomatrik ?></p>
+    <p>Kelas :<?=$kelas ?></p>
+    <p>Asal : Kedah</p>
+    <p>Jantina :Perempuan</p>
+
+    <img src="tasnim.jpg" alt="Gambar Profil" width="200">
+</body>
+</html>
+
